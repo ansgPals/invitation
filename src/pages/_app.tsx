245694e8dad3eff66c1f61cba19d42e998af67ce
hyemin.mun <iamm71kr@gmail.com>
2025-07-28 20:59:20 +0900

    임리하

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,26 +8,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>문혜민 | FE 개발자</title>
+        <title>임리하의 첫 생일파티에 초대드립니다.</title>
         <meta
           name="description"
-          content="프론트엔드 개발자 문혜민의 포트폴리오입니다."
+          content="임리하의 첫 생일파티에 초대드립니다."
         />
-        <meta name="author" content="문혜민" />
+        <meta name="author" content="임리하의 첫 생일파티에 초대드립니다." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="문혜민 | 프론트엔드 개발자" />
+        <meta
+          property="og:title"
+          content="임리하의 첫 생일파티에 초대드립니다."
+        />
         <meta
           property="og:description"
-          content="프론트엔드 개발자 문혜민입니다."
+          content="임리하의 첫 생일파티에 초대드립니다."
         />
         <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content="https://portfolio-opal-iota-92.vercel.app"
+          content="https://invitation-lyart-pi.vercel.app/lyha"
         />
         <meta
           property="og:image"
-          content="https://portfolio-opal-iota-92.vercel.app/idea.png"
+          content="https://portfolio-opal-iota-92.vercel.app/lyha/1.png"
         />
 
         <link rel="icon" href="/favicon.ico" />
